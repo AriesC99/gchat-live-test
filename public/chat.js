abe16783caf418a08a5ea848fa66bfa9fa227d00
AriesC99 <cardozomaverick99@gmail.com>
2021-01-18 22:16:36 +0530

    module-updates

--- public/chat.js
+++ public/chat.js
@@ -1,58 +1,55 @@
-var socket = io.connect('http://globallivechats.com/');
+var socket = io.connect("https://globalchat-live.herokuapp.com/");
 
-var message = document.getElementById('message'),
-	handle = document.getElementById('handle'),
-	btn = document.getElementById('send'),
-	output = document.getElementById('output'),
-	feedback = document.getElementById('feedback');
+var message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
 
-btn.addEventListener('click',function(){
-	socket.emit('chat',{
-		message: message.value,
-		handle: handle.value
-	});
+btn.addEventListener("click", function () {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
+  });
 });
 
-message.addEventListener('keypress',function(){
-	socket.emit('typing', handle.value);
+message.addEventListener("keypress", function () {
+  socket.emit("typing", handle.value);
 });
 
-socket.on('chat',function(data){
-
-	feedback.innerHTML = "";
-	output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message +'</p>';
-	message.innerHTML = "";
+socket.on("chat", function (data) {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+  message.innerHTML = "";
 });
 
-socket.on('typing',function(data){
-	feedback.innerHTML = '<p><em>' + data + ' is Typing a message....</em></p>';
+socket.on("typing", function (data) {
+  feedback.innerHTML = "<p><em>" + data + " is Typing a message....</em></p>";
 });
-
-
 
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
-	
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-	burger.addEventListener('click',()=>{
-		//toggle nav
-		nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
 
-		//Animate links
-	    navLinks.forEach((link, index) =>{
-		    if(link.style.animation){
-			   link.style.animation = ''
-		    }else{
-			   link.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 + 0.5}s`;
-		    }
-		});
-		//burger animation
-		burger.classList.toggle('toggle');
-
-	});
-	
-}
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
