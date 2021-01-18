b488e57179d3f2feaa06a0cdda9cecbd44aa8fcd
AriesC99 <cardozomaverick99@gmail.com>
2021-01-18 20:52:28 +0530

    added-server-file

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var socket = require('socket.io');
-
-
+var express = require("express");
+var socket = require("socket.io");
 var app = express();
-var server = app.listen(3000,function(){
-	console.log('Port is 3000');
-});
+var server = require("./server");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 var io = socket(server);
 
-io.on('connection', function(socket){
-	console.log('made socket connection', socket.id);
+io.on("connection", function (socket) {
+  console.log("made socket connection", socket.id);
 
-	socket.on('chat',function(data){
-		io.emit('chat',data);
-	});
+  socket.on("chat", function (data) {
+    io.emit("chat", data);
+  });
 
-	socket.on('typing',function(data){
-		socket.broadcast.emit('typing', data);
-	});
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
+
+module.exports = app;
